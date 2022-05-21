e248937aca64f359daf50ecb9cd7406c4982bbe1
JakeZalesny <jrzalesny@gmail.com>
2022-05-20 23:29:28 -0700

    updated add & delete options

--- MuscleGroup.py
+++ MuscleGroup.py
@@ -27,12 +27,12 @@ class MuscleGroup :
             muscle_group = input("What is the muscle group that you would like to add to? ") 
             workout = input("What is the workout you would like to add? ")
             
-            if muscle_group in muscle_group_list :
+            if muscle_group.title() in muscle_group_list :
                 values = (muscle_group, workout)
                 self.cursor.execute("""SELECT * FROM ? """, values)
                 workouts = self.cursor.fetchall()
                 
-                if workout not in workouts :
+                if workout.title() not in workouts :
                     self.cursor.execute("""INSERT INTO ? VALUES( , ?) """, values)
                 
                 else :
@@ -54,12 +54,12 @@ class MuscleGroup :
             muscle_group = input("What is the muscle group that you would like to delete from? ")
             workout = input("What is the workout you would like to delete? ")
             
-            if muscle_group in muscle_group_list :
+            if muscle_group.title() in muscle_group_list :
                 values = (muscle_group, workout)
                 self.cursor.execute("SELECT * FROM ?")
                 workouts = self.cursor.fetchall()
 
-                if workout in workouts :
+                if workout.title() in workouts :
                     self.cursor.execute("DELETE FROM ? WHERE workout_name LIKE %?%")
                 
                 else :
@@ -68,5 +68,7 @@ class MuscleGroup :
             else :
                 print("ERROR: Muscle Group does not exist")
                 delete_workout = True
+            
+            self.connection.commit()
 
 
